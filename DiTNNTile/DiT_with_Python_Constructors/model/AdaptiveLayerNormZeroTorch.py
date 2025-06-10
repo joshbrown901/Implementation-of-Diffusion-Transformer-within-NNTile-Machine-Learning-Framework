7f8fffa3fca378343921f480b4835590e800bd20
Joshua Udobang <54585664+joshbrown901@users.noreply.github.com>
2025-06-10 11:17:32 +0300

    Update AdaptiveLayerNormZeroTorch.py

--- DiTNNTile/DiT_with_Python_Constructors/model/AdaptiveLayerNormZeroTorch.py
+++ DiTNNTile/DiT_with_Python_Constructors/model/AdaptiveLayerNormZeroTorch.py
@@ -11,6 +11,7 @@ class AdaptiveLayerNormZeroTorch(nn.Module):
         self.W = nn.Parameter(torch.randn(6 * embedding_dim, embedding_dim))
         self.b = nn.Parameter(torch.randn(6 * embedding_dim)) if bias else None
         self.eps = 1e-6
+        self.norm = nn.LayerNorm(embedding_dim, elementwise_affine=False, eps = 1e-6)
 
     def linear(self, x):
         out = F.linear(x, self.W, self.b)
@@ -20,9 +21,5 @@ class AdaptiveLayerNormZeroTorch(nn.Module):
         activated_emb = F.silu(emb)
         lin_out = self.linear(activated_emb)
         shift_msa, scale_msa, gate_msa, shift_mlp, scale_mlp, gate_mlp = torch.chunk(lin_out, 6, dim=-1)
-        mean = x.mean(dim=-1, keepdim=True)
-        var = x.var(dim=-1, unbiased=False, keepdim=True)
-        norm_x = (x - mean) / torch.sqrt(var + self.eps)
-        x_out = norm_x * (1 + scale_msa[:, None]) + shift_msa[:, None]
-
+        x_out = self.norm(x) * (1 + scale_msa[:, None]) + shift_msa[:, None]
         return x_out, gate_msa, shift_mlp, scale_mlp, gate_mlp
